45d726169c73d71f2722714d3285508d8a202148
HarryDoesStuff <harryjlennox@live.co.uk>
2024-02-10 17:00:13 +0000

    Super basic reading from google cloud firestore

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-"""
-A sample Hello World server.
-"""
 import os
-
 from flask import Flask
+import firebase_admin
+from firebase_admin import firestore, credentials
 
-# pylint: disable=C0103
+# Use a service account.
+cred = credentials.Certificate('credentials.json')
+
+fb_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +18,22 @@ def hello_world():
 @app.route('/hello')
 def hello():
     return 'world!'
+
+@app.route('/getdocs')
+def docs():
+    index = db.collection("index")
+    docs = index.stream()
+    response = ""
+
+    for doc in docs:
+        response += (str(doc.id) + "<br>")
+        for hits in doc.to_dict().values():
+            for hit in hits:
+                response += (hit + "<br>")
+        response += "<br>"
+    return response
+
+
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
